4b2059951eb152cff3c49e5f1642fdfcdbaad890
kirya <cloudkserg11@gmail.com>
2018-09-24 17:10:18 +0300

    https://chronobank.atlassian.net/browse/MD-688 - parseInt to CHECK_SYSTEM

--- config/index.js
+++ config/index.js
@@ -76,7 +76,7 @@ let config = {
     exchange: process.env.SYSTEM_RABBIT_EXCHANGE || 'internal',
     serviceName: process.env.SYSTEM_RABBIT_SERVICE_NAME || 'system' 
   },
-  checkSystem: process.env.CHECK_SYSTEM || true,
+  checkSystem: process.env.CHECK_SYSTEM ? parseInt(process.env.CHECK_SYSTEM) : true,
   sidechainMongo,
   rest: {
     domain: process.env.DOMAIN || 'localhost',
